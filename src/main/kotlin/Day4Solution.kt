cae9defd0b0def50749a1bb09e80fc0649280c48
SpikyStars <arechestnutsspiky@gmail.com>
2023-12-04 18:36:01 -0800

    day 4 pt 2 solved

--- src/main/kotlin/Day4Solution.kt
+++ src/main/kotlin/Day4Solution.kt
@@ -26,6 +26,31 @@ class Day4Solution : Solution() {
     }
 
     override fun solvePt2() {
-        TODO("Not yet implemented")
+        var copiesPerCard = IntArray(input.size) {_ -> 1}
+
+        for (i in input.indices) {
+            val line = input[i]
+            var numOfMatching = 0
+
+            val numberLists = line.replace(Regex("Card \\d+:\\s+"), "").split("|")
+            val winningNums = numberLists[0].trim().replace(Regex("\\s+"), ",")
+                .split(",").toSet()
+            val yourNums = numberLists[1].trim().replace(Regex("\\s+"), ",").split(",")
+
+            // Count amount of matching numbers
+            for (num in yourNums) {
+                if (winningNums.contains(num)) {
+                    numOfMatching++
+                }
+            }
+//            println("Found $numOfMatching matches")
+
+            // Add copies of next few cards
+            for (j in 1 .. numOfMatching) {
+                copiesPerCard[i + j] += copiesPerCard[i]
+            }
+
+        }
+        println(copiesPerCard.fold(0) {s, e -> s + e})
     }
 }
