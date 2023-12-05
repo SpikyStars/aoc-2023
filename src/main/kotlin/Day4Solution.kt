154afa6bdfe3e3f78b52f7570b59347c558d1736
SpikyStars <arechestnutsspiky@gmail.com>
2023-12-04 17:59:42 -0800

    day 4 pt 1 solved

--- src/main/kotlin/Day4Solution.kt
+++ src/main/kotlin/Day4Solution.kt
@@ -1,6 +1,28 @@
 class Day4Solution : Solution() {
+
     override fun solvePt1() {
-        TODO("Not yet implemented")
+        var totalPoints = 0
+
+        for (line in input) {
+            var pointsForCard = 0
+            val numberLists = line.replace(Regex("Card \\d+:\\s+"), "").split("|")
+            val winningNums = numberLists[0].trim().replace(Regex("\\s+"), ",")
+                .split(",").toSet()
+            val yourNums = numberLists[1].trim().replace(Regex("\\s+"), ",").split(",")
+
+            for (num in yourNums) {
+                if (winningNums.contains(num)) {
+                    if (pointsForCard == 0) {
+                        pointsForCard = 1
+                    } else {
+                        pointsForCard *= 2
+                    }
+                }
+            }
+
+            totalPoints += pointsForCard
+        }
+        println(totalPoints)
     }
 
     override fun solvePt2() {
