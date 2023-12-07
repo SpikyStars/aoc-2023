b2e37376c82f8a2be6d162368a660f04730286db
SpikyStars <arechestnutsspiky@gmail.com>
2023-12-06 17:30:20 -0800

    day6 pt2 solved. good ol high school

--- src/main/kotlin/Day6Solution.kt
+++ src/main/kotlin/Day6Solution.kt
@@ -1,3 +1,8 @@
+import java.math.BigDecimal
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.sqrt
+
 class Day6Solution : Solution() {
 
     override fun solvePt1() {
@@ -24,16 +29,27 @@ class Day6Solution : Solution() {
 
     override fun solvePt2() {
         val raceTime = input[0].replace("Time:", "").trim()
-            .replace(Regex("\\s+"), "").toLong()
+            .replace(Regex("\\s+"), "").toDouble()
         val distRecord = input[1].replace("Distance:", "").trim()
-            .replace(Regex("\\s+"), "").toLong()
-        var marginOfError = 1
+            .replace(Regex("\\s+"), "").toDouble()
 
-//        var lowerBound =
-
-//        for (t in 0 .. raceTime) {
-//            if (t < )
-//        }
+        // Equation: (1)t^2 - (raceTime)t + distRecord <= 0
+        // (I think)
+        // (I haven't done this for years)
+        val bounds = solveQuadraticRounded(1.0, -raceTime, distRecord)
+        val lowerBound = bounds.first
+        val upperBound = bounds.second
+        // +1 to be inclusive
+        println(upperBound - lowerBound + 1)
     }
 
+    private fun solveQuadraticRounded(a : Double, b : Double, c : Double) : Pair<Long, Long> {
+        val discriminant = (b * b) - (4 * a * c)
+
+        val bound1 = (-b - sqrt(discriminant)) / (2 * a)
+        val bound2 = (-b + sqrt(discriminant)) / (2 * a)
+
+        return Pair(ceil(bound1).toLong(), floor(bound2).toLong())
+
+    }
 }
